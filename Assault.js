d7c155c95a8f8599aeaaad6c76258da2d0b86451
wdx888 <wdx1.13@qq.com>
2026-08-25 15:14:52 +0800

    Delete Assault.js file
    Remove Assault class and its documentation.

--- Assault.js
+++ Assault.js
@@ -1,16 +1,1 @@
-/**
- * 突击兵 - 保持200-300px距离，边走边射击
- */
 
-import { Enemy } from '../Enemy.js';
-
-export class Assault extends Enemy {
-  /**
-   * @param {Phaser.Scene} scene - 场景引用
-   * @param {number} x - 出生x坐标
-   * @param {number} y - 出生y坐标
-   */
-  constructor(scene, x, y) {
-    super(scene, x, y, 'assault');
-  }
-}
